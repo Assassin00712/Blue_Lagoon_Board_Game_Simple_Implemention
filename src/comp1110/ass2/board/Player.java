f5529809d51b3bd4eac57bbcf7581a75f51aae48
Xinwen Cao <u7305675@anu.edu.au>
2023-05-08 17:30:23 +1000

    attempt task 16

--- src/comp1110/ass2/board/Player.java
+++ src/comp1110/ass2/board/Player.java
@@ -3,6 +3,7 @@ package comp1110.ass2.board;
 import comp1110.ass2.BlueLagoon;
 import gittest.C;
 
+import static comp1110.ass2.BlueLagoon.calculateScores;
 import static comp1110.ass2.board.Coordinate.corFromString;
 import static comp1110.ass2.board.Coordinate.isLinked;
 
@@ -20,7 +21,6 @@ public class Player {
 
     public Player(int playId) {
         this.playId = playId;
-
     }
 
 
@@ -45,6 +45,16 @@ public class Player {
                 ", settlers=" + settlers +
                 ", villages=" + villages +
                 '}';
+    }
+
+    public static int playerScore(String stateString, int playId) {
+        int[] scores = calculateScores(stateString);
+        int score = scores[playId];
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public static Player playerFromString (String playerStates){
@@ -118,6 +128,23 @@ public class Player {
             players.add(playerFromString(playerString.strip()));
         }
         return players;
+    }
+
+    /**
+     * get which player is on phase
+     * @param gameStates State string provided
+     * @return the playId of the player who is playing
+     */
+    public static int getCurrentPlayer (String gameStates){
+        String[] states = gameStates.split("; ");
+        int playerId = 0;
+        for (String state : states){
+            if (state.startsWith("c")){
+                playerId = Integer.parseInt(String.valueOf(state.charAt(2)));
+                break;
+            }
+        }
+        return playerId;
     }
 
     /**
@@ -295,10 +322,10 @@ public class Player {
 
         score += resources.get(4) * 4;
 
-
         return score;
 
     }
+
 
     public static final String DEFAULT_GAME = "a 13 2; c 0 E; i 6 0,0 0,1 0,2 0,3 1,0 1,1 1,2 1,3 1,4 2,0 2,1; i 6 0,5 0,6 0,7 1,6 1,7 1,8 2,6 2,7 2,8 3,7 3,8; i 6 7,12 8,11 9,11 9,12 10,10 10,11 11,10 11,11 11,12 12,10 12,11; i 8 0,9 0,10 0,11 1,10 1,11 1,12 2,10 2,11 3,10 3,11 3,12 4,10 4,11 5,11 5,12; i 8 4,0 5,0 5,1 6,0 6,1 7,0 7,1 7,2 8,0 8,1 8,2 9,0 9,1 9,2; i 8 10,3 10,4 11,0 11,1 11,2 11,3 11,4 11,5 12,0 12,1 12,2 12,3 12,4 12,5; i 10 3,3 3,4 3,5 4,2 4,3 4,4 4,5 5,3 5,4 5,5 5,6 6,3 6,4 6,5 6,6 7,4 7,5 7,6 8,4 8,5; i 10 5,8 5,9 6,8 6,9 7,8 7,9 7,10 8,7 8,8 8,9 9,7 9,8 9,9 10,6 10,7 10,8 11,7 11,8 12,7 12,8; s 0,0 0,5 0,9 1,4 1,8 1,12 2,1 3,5 3,7 3,10 3,12 4,0 4,2 5,9 5,11 6,3 6,6 7,0 7,8 7,12 8,2 8,5 9,0 9,9 10,3 10,6 10,10 11,0 11,5 12,2 12,8 12,11; r C B W P S; " +
             "p 0 0 0 0 0 0 0 S T; p 1 42 1 2 3 4 5 S 5,6 8,7 T 1,2;";
