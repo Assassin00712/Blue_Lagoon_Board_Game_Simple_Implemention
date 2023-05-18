6ef2dcb90c611ac5eb6e461998b3425b635ad5e7
Wangtao Jia <u7370733@anu.edu.au>
2023-05-18 16:05:42 +1000

    Fix board

--- src/comp1110/ass2/board/Board.java
+++ src/comp1110/ass2/board/Board.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class Board {
 
     // The width of the board (left to right)
-    public static int BOARD_WIDTH;
+    public static int BOARD_WIDTH = 13;
     // The height of the board (top to bottom)
-    public final static int BOARD_HEIGHT = BOARD_WIDTH - 1;
+    public final static int BOARD_HEIGHT = 13;
 
     // The matrix of spots representing the board
     // For spotMatrix[row][col]:
